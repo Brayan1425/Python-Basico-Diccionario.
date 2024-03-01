9465d8b9bf1f799978497001b095a7d8a30d52d4
Brayan1425 <123999442+Brayan1425@users.noreply.github.com>
2024-02-29 23:15:00 -0500

    Panaderia

---
+++ main.py
@@ -0,0 +1,56 @@
+menu = dict(
+ "Pan Salados":
+   "Producto":list([
+    list{"nombre": "Baguette tradicional", "valor":"$" 3.50} ,
+    list {"nombre": "Pan de molde integral", "valor":"$" 5.50 }
+    list{"nombre": "Focaccia con romero y aceite de oliva", "valor":"$" 4.76 }
+    list{"nombre": "Chapata", "valor":"$" 2.56   }
+    list{"nombre": "Pan de centeno","valor":"$"  2.13  }
+    list{"nombre": "Pan de cebolla","valor":"$" 1.90  }
+    list{"nombre": "Pretzels","valor":"$" 1.85  }
+    list{"nombre": "Pan de ajo","valor":"$" 1.77}
+    list{"nombre": "Pan de maíz","valor":"$" 2.33 }
+    list{"nombre": "Bollos de pan con semillas","valor":"$" 2,89}
+]),
+"Promociones":([
+  {"codigo":3, "nombre", "compre 7", "valor": "$" 26,6}
+     {"codigo":8,"nombre","compre 9","valor": "$": 15,92}
+  ])
+)
+
+"Pan Azucar":
+    "Producto":list([
+        list{"nombre": "Rosquillas", "valor":"$" 2.50} ,
+        list{"nombre": "Croissants", "valor":"$" 4.50 }
+        list{"nombre": "Napolitanas", "valor":"$" 6.76 }
+        list{"nombre": "Dónutso", "valor":"$" 1.56   }
+        list{"nombre": "Magdalenas","valor":"$"  1.13  }
+        list{"nombre": "Tartaletas de frutas","valor":"$" 3.90  }
+        list{"nombre": "Bizcochos","valor":"$" 4.85  }
+        list{"nombre": "Ensaimadas","valor":"$" 3.77}
+        list{"nombre": "Galletas","valor":"$" 4.33 }
+        list{"nombre": "Crepes","valor":"$" 4,89}
+    ]),
+"Promociones2":(
+    {"codigo":3, "nombre","compre 7","valor": "$" 26,6}
+     {"codigo":8,"nombre","compre 9","valor": "$": 15,92}
+  )
+"Postres":list (
+    "Producto":list([
+    list{"nombre": "Tiramisú", "valor":"$" 5.50} ,
+    list {"nombre": "Cheesecake de fresa", "valor":"$" 3.50 }
+    list{"nombre": "Coulant de chocolate", "valor":"$" 5.76 }
+    list{"nombre": "Crème brûlée", "valor":"$" 3.56   }
+    list{"nombre": "Magdalenas","valor":"$"  5.13  }
+    list{"nombre": "Tartaletas de frutas","valor":"$" 8.90  }
+    list{"nombre": "Bizcochos","valor":"$" 6.85  }
+    list{"nombre": "Ensaimadas","valor":"$" 9.77}
+    list{"nombre": "Galletas","valor":"$" 3.33 }
+    list{"nombre": "Crepes","valor":"$" 2,89}
+    ])
+"Promociones3":({
+    {"codigo":3, "nombre","compre 7","valor": "$" 26,6}
+     {"codigo":8,"nombre","compre 9","valor": "$": 15,92}
+  })
+
+
